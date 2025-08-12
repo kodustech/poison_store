a6f16c61d6c08c02c4298f7f3fb46e598765a184
Junior Sartori <sartorijr92@gmail.com>
2025-08-12 20:35:31 -0300

    Adding new search

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :medication_requests, only: [:new, :create]
-  resources :reports, only: [] do
+  resources :reports, only: [:index] do
     collection do
       get :top_medications
+      get :search_doctors
     end
   end
   root 'medication_requests#new'
