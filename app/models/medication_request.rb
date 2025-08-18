4ceaf61ea87ba149fa1837311db81c83c1a2eb3c
Junior Sartori <sartorijr92@gmail.com>
2025-08-18 14:10:05 -0300

    Adding top medicaments with controller

--- app/models/medication_request.rb
+++ app/models/medication_request.rb
@@ -1,4 +1,25 @@
-class MedicationRequest < ApplicationRecord
+class MedicationRequest
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :name, type: String
+  field :cpf, type: String
+  field :cep, type: String
+  field :address, type: String
+  field :address_number, type: String
+  field :district, type: String
+  field :city, type: String
+  field :state, type: String
+  field :phone, type: String
+  field :email, type: String
+  field :monthly_income, type: BigDecimal
+  field :medication_name, type: String
+  field :prescription_photo, type: String
+  field :additional_info, type: String
+  field :doctor_crm, type: String
+  field :venous_application_notes, type: String
+  field :medication_quantity, type: BigDecimal
+
   validates :name, presence: true
   validates :cpf, presence: true, uniqueness: true
   validates :address, presence: true
